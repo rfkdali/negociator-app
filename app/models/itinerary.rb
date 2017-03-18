2c14d159e8d117ed1cdc96c865fcaa61ee2d249b
rfkdali <rfkdali@gmail.com>
2017-03-18 17:45:37 +0100

    Origin address is always Barbican office address

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,2 +1,9 @@
 class Itinerary < ApplicationRecord
+  def origin_latitude
+    51.520042
+  end
+
+  def origin_longitude
+    -0.098440
+  end
 end
--- spec/models/itinerary_spec.rb
+++ spec/models/itinerary_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Itinerary, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Origin address'
+    subject(:itinerary) { Itinerary.new }
+
+    office_address = {
+      origin_latitude: 51.520042,
+      origin_longitude: -0.098440
+    }
+
+    it 'is always Barbican office' do
+      expect(subject.origin_latitude).to eq(office_address[:origin_latitude])    
+      expect(subject.origin_longitude).to eq(office_address[:origin_longitude])    
+    end
 end
